37f860689a25c9ea50a1a6105c14b63221920e1f
Prashanth Kumar <prashanthgiri786@gmail.com>
2020-11-16 21:22:35 +0530

    pkk

--- web/index.php
+++ web/index.php
@@ -1,11 +1,23 @@
 <?php
 error_reporting(E_ALL);
+/*
+host 
 
+ec2-3-213-106-122.compute-1.amazonaws.com
+
+*/
 echo "hello world";
 
+echo "<pre>";
 
 $db = parse_url(getenv("DATABASE_URL"));
-var_dump($db);
-/*
-print_r("pgsql:" . sprintf("host=%s;port=%s;user=%s;password=%s;dbname=%s", $db["host"],$db["port"],$db["user"],$db["pass"],ltrim($db["path"]));
-*/
+try{
+	extract($db);
+	$dbname=ltrim($path);
+     $conn=PDO("pgsql:host=$host;port=$port;dbname=$dbname",$user,$pass);
+
+     var_dump($conn);
+}catch (Exception $e){
+	 echo $e;
+}
+echo "</pre>";
