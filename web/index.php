54646f684a64827bccda20854aa11a96ada2d62c
Prashanth Kumar <prashanthgiri786@gmail.com>
2020-11-16 19:44:25 +0530

    pk

--- web/index.php
+++ web/index.php
@@ -4,10 +4,10 @@ echo "hello world";
 
 #$db = parse_url(getenv("DATABASE_URL"));
 
-#print("pgsql:" . sprintf(
+/*print("pgsql:" . sprintf(
     "host=%s;port=%s;user=%s;password=%s;dbname=%s",
     $db["host"],
     $db["port"],
     $db["user"],
     $db["pass"],
-    ltrim($db["path"]);
+    ltrim($db["path"]);*/
