561ad5d6d126b0c4992f0940937c18c4f8db686f
prashanthKumar0 <giri00421@outlook.com>
2021-08-24 10:06:46 +0530

    cors fix

--- cors/index.php
+++ cors/index.php
@@ -1,5 +1,5 @@
 <?php
-header("Cross-Origin:*");
+header("Cross-Origin: *");
 $fileUrl=$_REQUEST['url'];
 $ch = curl_init($fileUrl);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -7,5 +7,5 @@ curl_exec($ch);
 
 # get the content type
 $mime=curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
-header("mime-type:$mime");
+header("Content-Type: $mime");
 echo file_get_contents($fileUrl);
