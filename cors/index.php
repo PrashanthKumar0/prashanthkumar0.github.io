91ff5417f4f16a276229f3a6fd466115886577d3
prashanthKumar0 <giri00421@outlook.com>
2021-08-24 10:04:08 +0530

    cors fix

--- cors/index.php
+++ cors/index.php
@@ -6,6 +6,6 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_exec($ch);
 
 # get the content type
-echo curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
-
-#echo file_get_contents($fileUrl);
+$mime=curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+header("mime-type:$mime");
+echo file_get_contents($fileUrl);
